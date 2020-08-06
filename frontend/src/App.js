07219f548479f21dcde19be964e7714782925b3e
jinho6225 <jinho6225@hotmail.com>
2020-08-06 00:28:44 -0700

    setting up structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import './App.css';
 
+
+
 function App() {
+
   return (
-    <div className="App">
-      hello world
+    <div className="container">
+      <header className="header">
+        <h2>Todo List</h2>
+        <h4>(Django RESTful api + React hooks)</h4>
+      </header>
+      <div className="inputBox">
+        <form method="POST" className="inputForm">
+          <input type="text" placeholder="Add task" className="inputField" name="title" />
+          <button type="submit" className="submitBtn">Submit</button>
+        </form>
+      </div>
+      <div className="list-container">
+        <ul className="unorder-list">
+          <li className="task-list">
+            <span className="content ">E-commerce web app</span>
+            <span className="icon">
+              <i className="far fa-edit" aria-hidden="true"></i>
+              <i className="far fa-trash-alt" aria-hidden="true"></i>
+            </span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
