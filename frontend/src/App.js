67f50b0e8cc6f43a01bfb5e7f919de0101127c54
jinho6225 <jinho6225@hotmail.com>
2020-08-07 00:01:42 -0700

    task-list render success

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,46 @@
 import React, { useState, useEffect }  from 'react';
+import ListOfTodo from './List/ListOfTodo'
 import './App.css';
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [taskList, setTaskList] = useState([])
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/task-list")
       .then((res) => res.json())
-      .then((data) => setTasks(data));
+      .then((data) => setTaskList(data));
   },[]);
 
+  
   return (
     <div className="container">
       <header className="header">
         <h2>Todo List</h2>
-        <h4>(Django RESTful api + React hooks)</h4>
+        <h4>(Django RESTful api + React Hooks)</h4>
       </header>
       <div className="inputBox">
         <form method="POST" className="inputForm">
-          <input type="text" placeholder="Add task" className="inputField" name="title" />
-          <button type="submit" className="submitBtn">Submit</button>
+          <input type="text" placeholder="Add task" className="inputField" />
+          <button type="submit" className="submitBtn" >Submit</button>
         </form>
       </div>
-      <div className="list-container">
-        <ul className="unorder-list">
-          {tasks.map((task) => {
-            return (
-              <li className="task-list" key={task.id}>
-                <span className="content ">{task.title}</span>
-                <span className="icon">
-                  <i className="far fa-edit" aria-hidden="true"></i>
-                  <i className="far fa-trash-alt" aria-hidden="true"></i>
-                </span>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <ListOfTodo taskList={taskList}/>
     </div>
   );
 }
---
+++ frontend/src/List/ListOfTodo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+const ListOfTodo = ({ taskList }) => {
+    
+    return (
+      <div className="list-container">
+        <ul className="unorder-list">
+
+            {taskList.length > 0 ? (
+                taskList.map(task => {
+                    const {id, completed, title} = task;
+                        return (
+                            <li className="task-list" key={id}>
+                                <span className="content ">{title}</span>
+                                <span className="icon">
+                                    <i className="far fa-edit" aria-hidden="true"></i>
+                                    <i className="far fa-trash-alt" aria-hidden="true"></i>
+                                </span>
+                            </li>
+                    )})
+                ) : (
+                    <li className="task-list" >
+                        <span className="content ">No List now, Enter your todo list</span>
+                        <span className="icon">
+                            <i className="far fa-edit" aria-hidden="true"></i>
+                            <i className="far fa-trash-alt" aria-hidden="true"></i>
+                        </span>
+                    </li>
+            )}
+
+        </ul>
+        </div>
+    )
+}
+
+
+
+export default ListOfTodo;
